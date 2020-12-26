e28ef9b47be68012470bafba25502154a349e318
Vinicius Diniz <vini_reolon@hotmail.com>
2020-12-26 08:39:45 -0300

    Acrescentada a pergunta sobre como voce estah

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -8,6 +8,7 @@ namespace TesteGit
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como voce esta hoje?");
 
             Console.ReadLine();
         }
